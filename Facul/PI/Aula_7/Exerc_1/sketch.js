1a91465ff7c75cc3087b1a62524ea95979877914
DANIEL ARISTOTELIS ASSUNCAO DA SILVA <daniel.aasilva1@senacsp.edu.br>
2024-04-26 21:00:13 -0300

    Ok

---
+++ Facul/PI/Aula_7/Exerc_1/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  frameRate(2);
+}
+
+function draw() {
+  if (mouseY <= width/2) {
+    background(random(0,255), random(0, 255), random(0, 255));
+  }
+}
